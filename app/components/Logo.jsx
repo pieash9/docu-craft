db00e3d1e86bee1dd2ee446876dae264138e28ea
Md. Pieash Ahmed <pieash9@gmail.com>
2025-07-01 22:53:44 +0600

    Add new SVG icons for contact, dark mode, hamburger, logo, right arrow, and search
    - Added contact.svg for user contact representation.
    - Introduced darkmode.svg for dark mode toggle functionality.
    - Included hamburger.svg for mobile navigation menu.
    - Added logo.svg for branding purposes.
    - Implemented rightArrow.svg for navigation indication.
    - Added search.svg for search functionality.

---
+++ app/components/Logo.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <div className="hidden lg:flex">
+      <Link aria-label="Home" href="/">
+        <Image
+          src="/logo.svg"
+          alt="Protocol"
+          className="h-6 w-auto"
+          width={100}
+          height={24}
+          priority
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-lighterDark text-9xl">lighterDark</div>
+    <div className="">lighterDark</div>
   );
 }
